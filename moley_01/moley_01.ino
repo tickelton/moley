b6a3fe609eb2be45ea238d62c046d3561518f872
tickelton <tickelton@gmail.com>
2018-05-05 21:35:56 +0200

    Fully funcional on ATMega328p

--- moley_01/moley_01.ino
+++ moley_01/moley_01.ino
@@ -1,22 +1,71 @@
-#define PIN_LED PD3
-#define PID_BUZ PD2
+#include <avr/sleep.h>
 
-long random_sleep;
+#define PIN_BUZ PD3
+#define PIN_LED PD2
 
-// the setup function runs once when you press reset or power the board
+int debug_led_counter = 0;
+
+int delay_map[] = {2, 4, 4, 8, 8, 16};
+//int delay_map[] = {2, 2, 2, 5, 5, 5};
+volatile int delay_count = 0;
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(PID_BUZ, OUTPUT);
+  setup_watchdog();
 }
 
-// the loop function runs over and over again forever
 void loop() {
 
-  while(1) {
-    random_sleep = random(1,6);
-    digitalWrite(PID_BUZ, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
-    digitalWrite(PID_BUZ, LOW);    // turn the LED off by making the voltage LOW
-    delay(random_sleep * 1000);    // wait for a second
+  // if watchdog has run 'delay_count' times
+  if (delay_count==0) {
+    // set new delay
+    delay_count = delay_map[random(0,6)];
+
+    // sound buzzer
+    pinMode(PIN_BUZ, OUTPUT);
+    digitalWrite(PIN_BUZ, HIGH);
+    if (debug_led_counter <2 ) {
+      pinMode(PIN_LED, OUTPUT);
+      digitalWrite(PIN_LED, HIGH);
+    }
+    
+    delay(1000);
+    
+    digitalWrite(PIN_BUZ, LOW);
+    pinMode(PIN_BUZ, INPUT);
+    if (debug_led_counter <2 ) {
+      digitalWrite(PIN_LED, LOW);
+      pinMode(PIN_LED, INPUT);
+      debug_led_counter++;
+    }
   }
+  
+  system_sleep();
+}
+
+// NOTE: Use WDTCSR for atmega328 and WDTCR for attiny85
+void setup_watchdog() {
+  // Reset 'Watchdoch Reset' flag
+  MCUSR &= ~(1<<WDRF);
+  // enable config mode
+  WDTCSR = (1<<WDCE)|(1<<WDE);
+  // start watchdog
+  //WDTCSR = (1<<WDIE) | (1<<WDP2) | (1<<WDP1);
+  WDTCSR = (1<<WDIE) | (1<<WDP3);
+}
+
+void system_sleep() {
+  // set mode to 'power down' and enable sleep mode
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+  
+  // do actual sleep
+  sleep_mode();
+
+  // execution continues here after watchdog
+  sleep_disable();
+}
+
+// Watchdog Timeout Interrupt
+ISR(WDT_vect) {
+  delay_count--;
 }
